ae983e029208abce91d5f9c90e863d3498043522
纪郅炀 <22373213@buaa.edu.cn>
2024-03-06 19:13:35 +0800

    3

--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -4,10 +4,13 @@ while [ $a -le 100 ]
 do
 	if [ $a -gt 70 ]	   #if loop variable is greater than 70
 	then
-
+		rm -rf flie$a
 	elif [ $a -gt 40 ]         # else if loop variable is great than 40
 	then
-
+	# 重命名为newfile
+		mv file$a newfile$a
+		
 	fi
 	           #don't forget change the loop variable
+	((a++))
 done
