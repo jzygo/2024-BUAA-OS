8c6f1c997272a36efa5199c8341bb961787eaf24
纪郅炀 <22373213@buaa.edu.cn>
2024-03-06 18:47:32 +0800

    1

--- src/palindrome.c
+++ src/palindrome.c
@@ -2,8 +2,14 @@
 int main() {
 	int n;
 	scanf("%d", &n);
-
-	if () {
+	//判断输入的数字是否是回文数
+	int m = n;
+	int r = 0;
+	while (m > 0) {
+		r = r * 10 + m % 10;
+		m = m / 10;
+	}
+	if (r==n) {
 		printf("Y\n");
 	} else {
 		printf("N\n");
