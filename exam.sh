2170a9e2509e6267a5c397c984362b4e755b5ecb
jzy <22373213@buaa.edu.cn>
2024-03-13 20:09:38 +0800

    5

--- exam.sh
+++ exam.sh
@@ -11,14 +11,14 @@ done
 gcc -o test/hello test/code/*.o
 ./test/hello 2>> ./test/err.txt
 mv test/err.txt .
-chmod 6431 err.txt
+chmod 6451 err.txt
 if [ $# -eq 2 ];
 then
-	n = $(($1 + $2))
+	n=$(($1+$2))
 	sed -n '${n}p' err.txt >&2
 elif [ $# -eq 1 ];
 then
-	n= $(($1+1))	
+	let n=$1+1	
 	sed -n '${n}p' err.txt >&2
 else
 	sed -n '2p' err.txt >&2
