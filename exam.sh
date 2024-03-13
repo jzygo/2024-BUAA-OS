61463323798728cf055ea70487a6d423453bf385
jzy <22373213@buaa.edu.cn>
2024-03-13 20:37:54 +0800

    v8

--- exam.sh
+++ exam.sh
@@ -2,7 +2,7 @@ mkdir test
 cp -r code test/
 cat code/14.c
 x=0
-end=15
+end=1
 while [ $x -ne 16 ]
 do
 	gcc -c test/code/${x}.c -o test/code/${x}.o
@@ -11,18 +11,16 @@ done
 gcc -o test/hello test/code/*.o
 ./test/hello 2>> ./test/err.txt
 mv test/err.txt .
-chmod a=x err.txt
-chmod o=xr err.txt
-chmod g=r err.txt
-chmod u=rw err.txt
+chmod 645 err.txt
+chmod a+x err.txt
 if [ $# -eq 2 ];
 then
 	n=$(($1+$2))
-	sed -n '${n}p' err.txt >&2
+	sed -n '${n}p' err.txt>&2
 elif [ $# -eq 1 ];
 then
-	let n=$1+1	
-	sed -n '${n}p' err.txt >&2
+	n=$(($1+$end))	
+	sed -n '${n}p' err.txt>&2
 else
-	sed -n '2p' err.txt >&2
+	sed -n '2p' err.txt>&2
 fi
