d75fda56b2d3468ddedadc4b8a662710fa0ac753
纪郅炀 <22373213@buaa.edu.cn>
2024-04-13 17:57:26 +0800

    v4

--- kern/env.c
+++ kern/env.c
@@ -249,7 +249,9 @@ int env_alloc(struct Env **new, u_int parent_id) {
 	e->env_user_tlb_mod_entry = 0; // for lab4
 	e->env_runs = 0;	       // for lab6
 	/* Exercise 3.4: Your code here. (3/4) */
-	asid_alloc(&(e->env_asid));
+	if ((r=asid_alloc(&(e->env_asid)))!=0) {
+		return r;
+	}
 	e->env_parent_id=parent_id;
 	/* Step 4: Initialize the sp and 'cp0_status' in 'e->env_tf'.
 	 *   Set the EXL bit to ensure that the processor remains in kernel mode during context
