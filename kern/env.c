c52aed9a80b74b6d397c5b4b93936b78b95a31c1
纪郅炀 <22373213@buaa.edu.cn>
2024-04-13 21:11:56 +0800

    v5

--- kern/env.c
+++ kern/env.c
@@ -295,7 +295,9 @@ static int load_icode_mapper(void *data, u_long va, size_t offset, u_int perm, c
 
 	/* Step 1: Allocate a page with 'page_alloc'. */
 	/* Exercise 3.5: Your code here. (1/2) */
-	page_alloc(&p);
+	if ((r=page_alloc(&p))!=0) {
+		return r;
+	}
 	/* Step 2: If 'src' is not NULL, copy the 'len' bytes started at 'src' into 'offset' at this
 	 * page. */
 	// Hint: You may want to use 'memcpy'.
