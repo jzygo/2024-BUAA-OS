b350f8b82791a640d4eb567a1c4e9c2619d764a8
纪郅炀 <22373213@buaa.edu.cn>
2024-05-29 11:38:22 +0000

    f1

--- tests/lab5_copy/main.c
+++ tests/lab5_copy/main.c
@@ -19,10 +19,10 @@ static char *path_dst = "/target";
 static char *file_dst = "/target/rr/test.rs";
 
 void test_copy_read(char *copy_src, char *copy_dst, char *src_file, char *dst_file, char *str) {
-	char buf1[2048], buf2[2048];
+	char buf1[8192], buf2[8192];
 	int r = open(src_file, O_RDWR);
 	os_assert(r >= 0, "failed to open %s, return value: %d", src_file, r);
-	read(r, buf1, 2048);
+	read(r, buf1, 8192);
 	close(r);
 
 	r = copy(copy_src, copy_dst);
@@ -30,14 +30,14 @@ void test_copy_read(char *copy_src, char *copy_dst, char *src_file, char *dst_fi
 
 	r = open(dst_file, O_RDWR);
 	os_assert(r >= 0, "failed to open %s, return value: %d", dst_file, r);
-	read(r, buf2, 2048);
+	read(r, buf2, 8192);
 	os_assert(strcmp(buf1, buf2) == 0, "copy failed, buf1 = %s, buf2 = %s", buf1, buf2);
 	int len = strlen(str);
 	write(r, str, len);
 	close(r);
 	r = open(src_file, O_RDWR);
 	os_assert(r >= 0, "failed to open %s, return value: %d", src_file, r);
-	read(r, buf2, 2048);
+	read(r, buf2, 8192);
 	os_assert0(strcmp(buf1, buf2) == 0, "write to dst result in src change");
 	debugf("OSTEST_OK\n");
 }
