375318a94e9dae38e3ae63d3442248760a6dbc1a
纪郅炀 <22373213@buaa.edu.cn>
2024-05-29 11:31:56 +0000

    v1

--- fs/serv.c
+++ fs/serv.c
@@ -294,7 +294,7 @@ void serve_remove(u_int envid, struct Fsreq_remove *rq) {
 }
 void serve_copy(u_int envid, struct Fsreq_copy *rq){
    // Lab 5-2-Exam: Your code here. (6/6)
-   int r;x
+   int r;
    r=directory_copy(rq->req_src_path,rq->req_dst_path);
    ipc_send(envid,r,NULL,0);
 }
