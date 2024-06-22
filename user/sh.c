0778da8d2597fff830a6eea58f36ab4af5916a1a
纪郅炀 <22373213@buaa.edu.cn>
2024-06-22 17:40:42 +0800

    v1

--- user/sh.c
+++ user/sh.c
@@ -235,12 +235,12 @@ void runcmd(char *s) {
 	}
 	argv[argc] = 0;
 	
-	int child = spawn(argv[0], argv);
-	//检查argv[0]中是否有.b，如果没有则在末尾加上.b
-	// Check if argv[0] contains ".b", if not, append ".b" to the end
 	if (strstr(argv[0], ".b") == NULL) {
 		strcat(argv[0], ".b");
 	}
+	int child = spawn(argv[0], argv);
+	//检查argv[0]中是否有.b，如果没有则在末尾加上.b
+	// Check if argv[0] contains ".b", if not, append ".b" to the end
 	close_all();
 	if (child >= 0) {
 		wait(child);
