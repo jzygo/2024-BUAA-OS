1cec838022fd1f7c1d6d92c3bb29e0b59deddaa8
纪郅炀 <22373213@buaa.edu.cn>
2024-06-22 20:26:05 +0800

    v1

--- user/touch.c
+++ user/touch.c
@@ -10,15 +10,15 @@ int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		usage();
 	}
-        debugf("adfad");
     if ((fd = open(argv[1], O_RDONLY)) >= 0) {
         close(fd);
     }
     else {        
-        if((fd=open(argv[1], O_CREAT | O_WRONLY))<0) {
+        if((fd=open(argv[1], O_CREAT))<0) {
             debugf("touch: cannot touch '%s': No such file or directory",argv[1]);
         }
         else {
+        debugf("adfad");
             close(fd);
         }
     }
