5abd7c26681dbf8c1fd68fd9aba52260c5897a53
纪郅炀 <22373213@buaa.edu.cn>
2024-06-22 20:19:04 +0800

    v1

---
+++ user/touch.c
@@ -0,0 +1,26 @@
+#include <lib.h>
+
+void usage(void) {
+	debugf("usage: touch file. Type in file which you want to create. Make sure that the path is exsisted.\n");
+	exit();
+}
+
+int main(int argc, char *argv[]) {
+	int fd;
+	if (argc < 2) {
+		usage();
+	}
+    if ((fd = open(argv[1], O_RDONLY)) >= 0) {
+        close(fd);
+    }
+    else {        
+        if((fd=open(t, O_CREAT | O_WRONLY))<0) {
+            debugf("touch: cannot touch '%s': No such file or directory",argv[1]);
+        }
+        else {
+            close(fd);
+        }
+    }
+	
+	return 0;
+}
