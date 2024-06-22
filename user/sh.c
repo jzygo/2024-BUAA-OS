6480ae45713ef584c4093ee12afc4a82e14c587d
纪郅炀 <22373213@buaa.edu.cn>
2024-06-22 23:43:26 +0800

    v1

--- user/sh.c
+++ user/sh.c
@@ -292,13 +292,13 @@ void runcmd(char *s) {
 	close_all();
 	if (child >= 0) {
 			debugf("start ipc_send1 \n");
-		wait(child);
 		int res = ipc_recv(NULL,0,0);
 		if (tag==1) {
 			debugf("start ipc_send2 \n");
 			ipc_send(syscall_get_parent(),res,NULL,0);
 		}
 			debugf("start ipc_send3 \n");
+		wait(child);
 	} else {
 		debugf("spawn %s: %d\n", argv[0], child);
 	}
