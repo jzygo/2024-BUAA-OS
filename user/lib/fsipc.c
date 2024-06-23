4c2a50cb1c6bb2bad283660af7ec44ad51fe6755
纪郅炀 <22373213@buaa.edu.cn>
2024-06-23 10:18:14 +0800

    v1

--- user/lib/fsipc.c
+++ user/lib/fsipc.c
@@ -23,6 +23,7 @@ static int fsipc(u_int type, void *fsreq, void *dstva, u_int *perm) {
 	u_int whom;
 	// Our file system server must be the 2nd env.
 	ipc_send(envs[1].env_id, type, fsreq, PTE_D);
+	debugf("create %s\n", rq->req_path);
 	return ipc_recv(&whom, dstva, perm);
 }
 
