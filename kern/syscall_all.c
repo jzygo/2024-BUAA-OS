033b0b6347d466216fa59d2eff221e647b575b09
纪郅炀 <22373213@buaa.edu.cn>
2024-06-24 18:46:57 +0800

    v1

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -62,7 +62,7 @@ u_int sys_add_job(char *name) {
 }
 
 u_int sys_add_job_name(int index,char a) {
-	for (int i =0 i<job_num; i++) {
+	for (int i =0;i<job_num; i++) {
 		if (jobs[i] == curenv->env_id) {
 			job_name[i][index]=a;
 		}
