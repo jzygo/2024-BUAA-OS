8f9502cf3a29f10057af1cf9fea5304be5e43417
纪郅炀 <22373213@buaa.edu.cn>
2024-06-24 14:07:37 +0800

    v1

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -17,12 +17,12 @@ void ipc_send(u_int whom, u_int val, const void *srcva, u_int perm) {
 	int cnt =0;
 	while ((r = syscall_ipc_try_send(whom, val, srcva, perm)) == -E_IPC_NOT_RECV) {
 		syscall_yield();
-		cnt++;
-		if (cnt > 1000) {
-			r=0;
-			debugf("shut down. envid:%d;parentid:%d\n",syscall_getenvid(),syscall_get_parent());
-			break;
-		}
+		// cnt++;
+		// if (cnt > 1000) {
+		// 	r=0;
+		// 	debugf("shut down. envid:%d;parentid:%d\n",syscall_getenvid(),syscall_get_parent());
+		// 	break;
+		// }
 	}
 	user_assert(r == 0);
 }
