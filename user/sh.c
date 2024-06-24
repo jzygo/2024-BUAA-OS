849012812272300c827ceeb70980e93703736193
纪郅炀 <22373213@buaa.edu.cn>
2024-06-24 17:44:59 +0800

    v1

--- user/sh.c
+++ user/sh.c
@@ -178,8 +178,8 @@ int parsecmd(char **argv, int *rightpipe) {
 			if(son==0) {
 				// debugf("son=%d, & \n",son);
 				waitNew=0;
-				debugf("instr:%s\n",buf_before);
-				syscall_add_job(buf_before);
+				debugf("instr:%s\n",buf);
+				syscall_add_job(buf);
 				return argc;
 			} 
 			else {
