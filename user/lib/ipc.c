2681684e4f048b55de0bc471ae0f9d8edfe6fea5
纪郅炀 <22373213@buaa.edu.cn>
2024-06-24 13:25:17 +0800

    v1

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -11,9 +11,9 @@
 // Hint: use syscall_yield() to be CPU-friendly.
 void ipc_send(u_int whom, u_int val, const void *srcva, u_int perm) {
 	int r;
+		debugf("ipc_send_yield,envid:%d\n;parentid:%d",syscall_getenvid(),syscall_get_parent());
 	while ((r = syscall_ipc_try_send(whom, val, srcva, perm)) == -E_IPC_NOT_RECV) {
 		syscall_yield();
-		debugf("ipc_send_yield,envid:%d\n",syscall_getenvid());
 	}
 	user_assert(r == 0);
 }
