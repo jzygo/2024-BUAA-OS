3342237c7f654df075c310b02539588cd8a6e12c
纪郅炀 <22373213@buaa.edu.cn>
2024-06-24 13:25:59 +0800

    v1

--- user/sh.c
+++ user/sh.c
@@ -366,7 +366,7 @@ void runcmd(char *s) {
 	int child = spawn(p, argv);
 	close_all();
 	if (child >= 0) {
-		debugf("spawn %s: %d\n", p, child);
+		debugf("childenv=%d\n", p, child);
 		debugf("nowenv=%d\n",syscall_getenvid());
 		u_int caller;
 		int res = ipc_recv(&caller,0,0);
